81200db82da5b9c2aaccb080dfef72214f70e0ed
huntu09 <memeksadas@gmail.com>
2025-06-18 16:01:02 +0700

    hapussemua

--- scripts/05-create-admin.sql
+++
@@ -1,4 +0,0 @@
--- Create admin user (password: admin123)
-INSERT INTO admin_users (email, password_hash, name, role) VALUES
-('admin@airdrophunter.com', '$2b$10$rQZ9vKzqzQzqzQzqzQzqzOeKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK', 'Admin User', 'admin')
-ON CONFLICT (email) DO NOTHING;
